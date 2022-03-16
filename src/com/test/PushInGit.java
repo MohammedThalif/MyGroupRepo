53b7160a2c754be5fd5ee1ccfbb87f3dae9db0a6
MohammedThalif <thalifal@gmail.com>
2022-03-16 12:23:09 +0530

    new line

--- src/com/test/PushInGit.java
+++ src/com/test/PushInGit.java
@@ -5,6 +5,8 @@ public class PushInGit {
 	public static void main(String[] args) {
 		System.out.println("First time Login GIT");
 		System.out.println("Updated");
+		System.out.println("New Code Added");
+		System.out.println("Line 1");
 	}
 
 }
